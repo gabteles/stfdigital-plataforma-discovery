67fa9dd951f86c942bb338912cdd5f4554bed954
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-22 00:56:39 -0300

    Voltando para o spring boot 1.3.5.
    Ocorreu um problema quando o discovery chamava o endpoint userauthentication/user (ao chamar a api de tarefas). Ele enviava peticionamento como Principal, o que causava um CastClastException.

--- src/test/java/br/jus/stf/core/component/ComponentIntegrationTests.java
+++ src/test/java/br/jus/stf/core/component/ComponentIntegrationTests.java
@@ -11,10 +11,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -27,10 +27,10 @@ import br.jus.stf.core.integrationtest.ContextInitializer;
  * @author lucas.rodrigues
  *
  */
-@RunWith(SpringRunner.class)
-@AutoConfigureMockMvc
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebIntegrationTest({"server.port:0"})
+@SpringApplicationConfiguration({ContextInitializer.class, MockEurekaRegistry.class})
 @ActiveProfiles("test")
-@SpringBootTest(value = {"server.port:0"}, classes = {ContextInitializer.class, MockEurekaRegistry.class})
 public class ComponentIntegrationTests {
 
     @Autowired
